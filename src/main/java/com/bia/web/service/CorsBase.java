2e7082d69ec90c6bfa08bf8463e26fda681fe692
qinyongchao@banggood.com <qinyongchao@banggood.com>
2020-09-14 19:05:48 +0800

    *

--- src/main/java/com/bia/web/service/CorsBase.java
+++ src/main/java/com/bia/web/service/CorsBase.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @CrossOrigin(
-        origins = "",
-        allowCredentials = "true",
-        allowedHeaders = "*",
-        methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE,RequestMethod.OPTIONS,RequestMethod.HEAD}
+    origins = "http://localhost:8080",
+    allowCredentials = "true",
+    allowedHeaders = "*",
+    methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE,RequestMethod.OPTIONS,RequestMethod.HEAD}
 )
 public class CorsBase {
 
