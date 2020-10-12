6901176910a9052528cd42ac7c24f24b0af7a695
qinyongchao@banggood.com <qinyongchao@banggood.com>
2020-10-12 16:07:54 +0800

    *

--- src/main/java/com/bia/web/Application.java
+++ src/main/java/com/bia/web/Application.java
@@ -2,10 +2,22 @@ package com.bia.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
+    }
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.bia.web.service")).build();
     }
 }
